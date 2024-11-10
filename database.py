c0a6f5fd0ea9212b25acdd70945f5e0a19686117
7immin <155518303+7immin@users.noreply.github.com>
2024-11-10 21:50:46 +0900

    Add files via upload

---
+++ database.py
@@ -0,0 +1,57 @@
+import pyrebase
+import json
+
+class DBhandler:
+    def __init__(self):
+        with open('./authentication/firebase_auth.json') as f:
+            config=json.load(f)
+        firebase = pyrebase.initialize_app(config)
+        self.db = firebase.database() 
+
+    def insert_item(self, name, data, img_path):
+        item_info ={
+        "name": data['name'],
+        "price": data['price'],
+        "category": data['category'],
+        "payment": data['payment'],
+        "stock": data['stock'],
+        "seller": data['seller'],
+        "phone": data['phone'],
+        "addr": data['addr'],
+        "info": data['info'],
+        "opt": data['opt'],
+        "img_path": img_path
+        }
+        self.db.child("item").child(name).set(item_info)
+        print(data,img_path)
+        return True
+    
+    def insert_user(self, data, pw):
+        user_info ={
+            "id": data['id'],
+            "pw": pw,
+            "nickname": data['nickname'],
+            "email": data['email'],
+            "name": data['name'],
+            "position": data['position'],
+            "phone": data['phone']
+        }
+        if self.user_duplicate_check(str(data['id'])):
+            self.db.child("user").push(user_info)
+            print(user_info)
+            return True
+        else:
+            return False
+    
+    def user_duplicate_check(self, id_string):
+            users = self.db.child("user").get()
+            print("users###",users.val())
+            if str(users.val()) == "None": # first registration
+                return True
+            else:
+                for res in users.each():
+                    value = res.val()
+
+                    if value['id'] == id_string:
+                        return False
+            return True
